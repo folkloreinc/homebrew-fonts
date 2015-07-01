47cb02a48f2734f77008493f497825adf4ff277e
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2015-07-01 10:50:15 -0400

    Better

---
+++ sfnt2woff.rb
@@ -0,0 +1,30 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Sfnt2woff < Formula
+  homepage "http://people.mozilla.org/~jkew/woff/"
+  url "http://people.mozilla.org/~jkew/woff/woff-code-latest.zip"
+  sha1 "59879f1bdeeafce7fc9d4b51406e80d7a4cd0293"
+  version "2014-12-18"
+  sha256 "7713630d2f43320a1d92e2dbb014ca3201caab1dd4c0ab92816016824c680d96"
+
+  def install
+    system "make"
+    bin.install "sfnt2woff"
+    bin.install "woff2sfnt"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test sfnt2woff`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ woff2.rb
@@ -0,0 +1,34 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Woff2 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/google/woff2.git"
+  version "2"
+  sha256 ""
+
+  # depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # system "cmake", ".", *std_cmake_args
+    system "make", "clean", "all" # if this fails, try separate make/make install steps
+    bin.install "woff2_compress"
+    bin.install "woff2_decompress"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test woff`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
